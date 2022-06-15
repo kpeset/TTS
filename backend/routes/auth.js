116b78764ef5fab4524790e3b916af13dd77d126
Kevin Peset <kevin.peset@gmail.com>
2022-06-15 21:36:29 +0200

    Ajout du hashage des passwords

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,21 +20,32 @@ if(emailExist) return res.status(400).send("Votre email appartient déjà à un 
 
 // Il est important de hasher le password de l'utilisateur. Je vais utiliser la bibliothèque Argon2 car recommandé par l'OWASP.
 
-
-
-
+const hashingOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1
+  };
+  
+const hashedPassword = await argon2.hash(req.body.password, hashingOptions);
+  
+  
 // Si il n'y a pas d'erreur suite à la vérification via Joi alors il passe à l'étape suivante qui est de créer le user en question
   const user = new User({
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+// Je l'ui demande d'afficher dans postman UNIQUEMENT l'id de l'utilisateur
+    res.send({user: user._id});
     console.log("L'utilisateur a bien été ajouté à la base de données !");
   } catch (err) {
     res.status(400).send(err)
   }
 });
+
+// PARTIE LOGIN
+
 
 module.exports = router;
