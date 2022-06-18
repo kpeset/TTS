e536896c3515bcaf9ea285fdf764d78e221c8502
Kevin Peset <kevin.peset@gmail.com>
2022-06-18 19:11:11 +0200

    correction de fautes

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -10,21 +10,21 @@ function About() {
 
         <p>
           Précédemment, j'étais responsable d'un magasin de cigarette
-          électronique à Paris, mais j'ai dû quitter cette société pour la
-          raison dite précédemment. C'était une expérience vraiment incroyable
+          électronique à Paris, mais j'ai dû quitter cette société pour suivre
+          ma compagne en Belgique. C'était une expérience vraiment incroyable
           car cela m'a permis de travailler avec encore plus d'autonomie et de
-          créativité par rapport à mes expériences précédentes dans le secteur
-          hôtelier dans lequel j'ai une expérience professionnelle à
-          l'international.
+          créativité par rapport à mes expériences précédentes dans
+          l'hôtellerie, secteur dans lequel j'ai une expérience professionnelle
+          à l'international.
         </p>
         <p>
           Suite à ce départ, j'ai décidé de continuer sur la voie de
           l'épanouissement personnel. Il était donc évident de me reconvertir
           dans le développement web car j'ai toujours eu une grande appétence
-          pour la programmation et la résolution de problèmes. C'est en février
-          que j'ai entamé ma reconversion à la Wild Code School, un boot camp de
-          5 mois orienté projet, dans lequel j'ai choisi de suivre le cursus
-          React.js et Node.js.
+          pour la programmation. C'est en février que j'ai entamé ma
+          reconversion à la Wild Code School, un boot camp de 5 mois orienté
+          projet, dans lequel j'ai choisi de suivre le cursus React.js et
+          Node.js.
         </p>
         <p>
           Ce métier me plaît énormément car j'aime apprendre constamment,
@@ -34,7 +34,7 @@ function About() {
         </p>
         <p>
           Si vous voulez en savoir plus mon parcours professionnel, je vous
-          invite à consulter mon Linkedin
+          invite à consulter mon Linkedin.
         </p>
         <a
           target="_blank"
